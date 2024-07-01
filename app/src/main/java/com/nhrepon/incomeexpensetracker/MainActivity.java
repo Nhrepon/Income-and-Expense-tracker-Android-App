242caf45739c722705f8bb0bc33dcb47616e7bc3
nhrepon <nurhossainrepon7248@gmail.com>
2024-07-01 21:32:46 +0600

    version:1.0.0

---
+++ app/src/main/java/com/nhrepon/incomeexpensetracker/MainActivity.java
@@ -0,0 +1,46 @@
+package com.nhrepon.incomeexpensetracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TextView welcome;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        welcome=findViewById(R.id.welcome);
+
+        Thread thread=new Thread(){
+            public void run() {
+                try {
+                    sleep(2000);
+                    startActivity(new Intent(MainActivity.this, Home.class));
+                }catch ( Exception e ) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+
+//        welcome.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(MainActivity.this, Home.class));
+//            }
+//        });
+
+
+
+
+
+    }
+}
